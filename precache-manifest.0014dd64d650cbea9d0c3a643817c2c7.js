ebf5894835f59dc901b590e5d3ef9038889697c8
idkmyusername <tarasrusnakbelarus@gmail.com>
2020-12-30 13:23:02 +0300

    Updates

---
+++ precache-manifest.0014dd64d650cbea9d0c3a643817c2c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcaf109096d2a68f59b87b06bae35fd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17fe484b92220afd22b4",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "29f20132502f79b7fdb2",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "17fe484b92220afd22b4",
+    "url": "/static/js/2.f0491e57.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.f0491e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f20132502f79b7fdb2",
+    "url": "/static/js/main.7daaa863.chunk.js"
+  },
+  {
+    "revision": "f79bee5381e610514cf0",
+    "url": "/static/js/runtime-main.6d91881a.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
